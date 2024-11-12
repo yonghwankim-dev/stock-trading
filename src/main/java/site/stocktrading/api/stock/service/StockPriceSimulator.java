4c5f25f4b52801b0bc14c2e9c33b2030df77d42f
yonghwankim-dev <dragonbead95@naver.com>
2024-11-12 12:00:32 +0900

    refactor: InterruptedException 예외 처리

--- src/main/java/site/stocktrading/api/stock/service/StockPriceSimulator.java
+++ src/main/java/site/stocktrading/api/stock/service/StockPriceSimulator.java
@@ -1,7 +1,5 @@
 package site.stocktrading.api.stock.service;
 
-import java.util.List;
-import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -9,41 +7,43 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import site.stocktrading.api.stock.domain.Stock;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class StockPriceSimulator {
 
 	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-	private final Random random = new Random();
 	private final StockService service;
 	private final RangeRandomPercentageGenerator generator;
 
-	public void startSimulation(){
+	public void startSimulation() {
 		scheduler.scheduleAtFixedRate(this::updatePrices, 0, 1, TimeUnit.SECONDS);
 	}
 
 	private void updatePrices() {
 		service.findAll().stream()
-			.map(stock->stock.newStock(generator))
+			.map(stock -> stock.newStock(generator))
 			.map(service::saveStock)
 			.forEach(this::printStock);
 	}
 
-	private void printStock(Stock stock){
+	private void printStock(Stock stock) {
 		System.out.printf("Updated price to %s%n", stock);
 	}
 
-	// 시뮬레이션 종료
-	public void stopSimulation(){
+	public void stopSimulation() {
 		scheduler.shutdown();
-		try{
+		try {
 			if (!scheduler.awaitTermination(1, TimeUnit.SECONDS)) {
 				scheduler.shutdownNow();
 			}
 		} catch (InterruptedException e) {
+			log.warn("Interrupted!", e);
 			scheduler.shutdownNow();
+			Thread.currentThread().interrupt();
 		}
 	}
 }
