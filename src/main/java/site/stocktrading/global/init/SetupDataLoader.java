ba10dcb13e15e4be8e9e3f87a50ed27f3d6b853b
yonghwankim-dev <dragonbead95@naver.com>
2024-11-12 12:24:41 +0900

    feat: setupDataLoader 구현

--- src/main/java/site/stocktrading/global/init/SetupDataLoader.java
+++ src/main/java/site/stocktrading/global/init/SetupDataLoader.java
@@ -6,18 +6,26 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import site.stocktrading.api.stock.service.StockFileReader;
+import site.stocktrading.api.stock.service.StockParser;
+import site.stocktrading.api.stock.service.StockService;
 
 @Profile(value = {"!test"})
 @Component
 @RequiredArgsConstructor
 public class SetupDataLoader implements ApplicationListener<ContextRefreshedEvent> {
 	private boolean alreadySetup = false;
+	private final StockFileReader reader;
+	private final StockParser parser;
+	private final StockService service;
 
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		if (alreadySetup) {
 			return;
 		}
+		parser.parseStocks(reader.readStockFile("stocks.csv"))
+			.forEach(service::saveStock);
 		alreadySetup = true;
 	}
 }
