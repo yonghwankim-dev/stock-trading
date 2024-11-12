57d6ac2cbe3b939e9caa741a681113b800cd6dcc
yonghwankim-dev <dragonbead95@naver.com>
2024-11-12 10:40:03 +0900

    initial commit

---
+++ src/test/java/site/stocktrading/StockTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package site.stocktrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
