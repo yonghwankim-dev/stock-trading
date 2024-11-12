ea40b0e11f12be29a77dd582c77b8521e58c777d
yonghwankim-dev <dragonbead95@naver.com>
2024-11-12 11:25:37 +0900

    feat: Stock toString 재정의

--- src/main/java/site/stocktrading/api/stock/domain/Stock.java
+++ src/main/java/site/stocktrading/api/stock/domain/Stock.java
@@ -3,15 +3,22 @@ package site.stocktrading.api.stock.domain;
 import java.util.Map;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @EqualsAndHashCode
+@Getter
 public class Stock {
 	private final String name;
 	private final int price;
 
 	public Stock saveTo(Map<String, Stock> store) {
 		return store.put(name, this);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("name=%s, price=%d", name, price);
 	}
 }
