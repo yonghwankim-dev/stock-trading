4d28306f2a3c252685a68fee43712efdda958495
yonghwankim-dev <dragonbead95@naver.com>
2024-11-13 15:08:05 +0900

    refactor: quantity 매개변수화

--- src/main/java/site/stocktrading/api/broker/domain/Broker.java
+++ src/main/java/site/stocktrading/api/broker/domain/Broker.java
@@ -7,8 +7,7 @@ import site.stocktrading.api.stock.domain.Stock;
 import site.stocktrading.api.trade.domain.Order;
 
 public class Broker {
-	public Order orderBuyStock(Account buyer, Stock stock) {
-		int quantity = 5;
+	public Order orderBuyStock(Account buyer, Stock stock, int quantity) {
 		LocalDateTime tradeTime = LocalDateTime.of(2024, 11, 13, 12, 0, 0);
 		return Order.buy(buyer, stock, quantity, tradeTime);
 	}
--- src/test/java/site/stocktrading/api/broker/domain/BrokerTest.java
+++ src/test/java/site/stocktrading/api/broker/domain/BrokerTest.java
@@ -19,10 +19,10 @@ class BrokerTest {
 		Account account = new Account(1L);
 		Broker broker = new Broker();
 		Stock samsung = new Stock("삼성전자보통주", 50000);
-		// when
-		Order order = broker.orderBuyStock(account, samsung);
-		// then
 		int quantity = 5;
+		// when
+		Order order = broker.orderBuyStock(account, samsung, quantity);
+		// then
 		LocalDateTime tradeTime = LocalDateTime.of(2024, 11, 13, 12, 0, 0);
 		Order expected = Order.buy(account, samsung, quantity, tradeTime);
 		Assertions.assertThat(order).isEqualTo(expected);
