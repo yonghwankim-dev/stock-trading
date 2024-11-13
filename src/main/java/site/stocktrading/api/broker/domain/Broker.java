02b156ef92dc3e8da2f38dc763f8ac87bd71acf1
yonghwankim-dev <dragonbead95@naver.com>
2024-11-13 14:56:54 +0900

    test: Order 인스턴스 생성

--- src/main/java/site/stocktrading/api/broker/domain/Broker.java
+++ src/main/java/site/stocktrading/api/broker/domain/Broker.java
@@ -1,9 +1,15 @@
 package site.stocktrading.api.broker.domain;
 
+import java.time.LocalDateTime;
+
+import site.stocktrading.api.stock.domain.Stock;
 import site.stocktrading.api.trade.domain.Order;
 
 public class Broker {
 	public Order orderBuyStock() {
-		return null;
+		Stock stock = new Stock("삼성전자보통주", 50000);
+		int quantity = 5;
+		LocalDateTime tradeTime = LocalDateTime.of(2024, 11, 13, 12, 0, 0);
+		return Order.buy(stock, quantity, tradeTime);
 	}
 }
--- src/test/java/site/stocktrading/api/broker/domain/BrokerTest.java
+++ src/test/java/site/stocktrading/api/broker/domain/BrokerTest.java
@@ -1,22 +1,29 @@
 package site.stocktrading.api.broker.domain;
 
+import java.time.LocalDateTime;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import site.stocktrading.api.stock.domain.Stock;
 import site.stocktrading.api.trade.domain.Order;
 
 class BrokerTest {
 
 	@DisplayName("브로커에게서 종목을 매수 주문한다")
 	@Test
-	void buy() {
+	void orderBuyStock() {
 		// given
 		Broker broker = new Broker();
 		// when
 		Order order = broker.orderBuyStock();
 		// then
-		Assertions.assertThat(order).isNull();
+		Stock samsung = new Stock("삼성전자보통주", 50000);
+		int quantity = 5;
+		LocalDateTime tradeTime = LocalDateTime.of(2024, 11, 13, 12, 0, 0);
+		Order expected = Order.buy(samsung, quantity, tradeTime);
+		Assertions.assertThat(order).isEqualTo(expected);
 	}
 
 }
