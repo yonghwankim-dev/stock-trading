0808a41dd04cf0f1d5feaabfb31b383c86b91e8e
yonghwankim-dev <dragonbead95@naver.com>
2024-11-13 10:28:29 +0900

    feat: 거래 체결 서비스 메서드 구현

--- src/main/java/site/stocktrading/api/trade/service/TradeService.java
+++ src/main/java/site/stocktrading/api/trade/service/TradeService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import site.stocktrading.api.stock.domain.Stock;
 import site.stocktrading.api.trade.domain.Order;
+import site.stocktrading.api.trade.domain.Trade;
 import site.stocktrading.global.util.delay.DelayService;
 import site.stocktrading.global.util.time.TimeService;
 
@@ -35,6 +36,22 @@ public class TradeService {
 			// 주문 처리 성공
 			LocalDateTime tradeTime = timeService.now();
 			return Order.sell(stock, quantity, tradeTime);
+		});
+	}
+
+	// 매수와 매도 주문을 비동기적으로 처리 후 결과를 받아오는 예시
+	public CompletableFuture<Trade> processOrders(Stock stock, int quantity) {
+		CompletableFuture<Order> buyFuture = buyStock(stock, quantity);
+		CompletableFuture<Order> sellFuture = sellStock(stock, quantity);
+
+		return CompletableFuture.allOf(buyFuture, sellFuture).thenApplyAsync(unused -> {
+			try {
+				Order buyOrder = buyFuture.get();
+				Order sellOrder = sellFuture.get();
+				return new Trade(buyOrder, sellOrder);
+			} catch (Exception e) {
+				throw new TradeException("fail trade", e);
+			}
 		});
 	}
 }
